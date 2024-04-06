dfd74bf3b6ca4efe27e3ca0c9e4c1fb6034e77ab
Sam Dicken <sam.dicken@outlook.com>
2024-04-06 21:05:05 +0100

    My computer specs list

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,15 +6,22 @@ namespace RazorPages.Pages;
 
 public class IndexModel : PageModel
 {
+    public string Processor = "AMD Ryzen 9 3900X";
+    public string Memory = "IRDM 32GB 3600MHz DDR4";
+    public string Graphics = "Nvidia RTX 3080";
+    public string Motherboard = "Gigabyte X570 Aorus Master";
+    public string Power = "Corsair 850W 80+Gold";
+    public string Case = "Jonsbo UMX4 Mid Tower";
+    public string[,] Storage = { {"Disk 1","Samsung 970 Evo Plus 1TB"}, {"Disk 2","Seagate FireCuda 520 2TB"}, {"Disk 3","Seagate IronWolf 3TB"},};
+    public void OnGet()
+    {
+        
+    }
+
     private readonly ILogger<IndexModel> _logger;    
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
-    }
-
-    public void OnGet()
-    {
-        
     }
 }
