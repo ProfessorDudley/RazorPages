fbb4a5615e0e08d6090483124921bbc5d765e496
Sam Dicken <sam.dicken@outlook.com>
2024-04-21 18:13:54 +0100

    MS Razor Pages Tutorial

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,27 +5,15 @@ namespace RazorPages.Pages;
 
 public class IndexModel : PageModel
 {
-    public string Processor = "AMD Ryzen 9 3900X";
-    public string Memory = "IRDM 32GB 3600MHz DDR4";
-    public string Graphics = "Nvidia RTX 3080";
-    public string Motherboard = "Gigabyte X570 Aorus Master";
-    public string Power = "Corsair 850W 80+Gold";
-    public string Case = "Jonsbo UMX4 Mid Tower";
-    public string[] Storage = { "Samsung 970 Evo Plus 1TB", "Seagate FireCuda 520 2TB", "Seagate IronWolf 3TB"};
-    public int Count = 0;
-    public void OnGet()
-    {
-        
-    }
-    public void OnPost()
-    {
-        Count++;
-    }
-
-    private readonly ILogger<IndexModel> _logger;    
+    private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,51 +1,30 @@
-// using RazorPages.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+var builder = WebApplication.CreateBuilder(args);
 
-// var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
-// // Add services to the container.
-// builder.Services.AddRazorPages();
+var app = builder.Build();
 
-// var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-// // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-// app.UseHttpsRedirection();
-// app.UseStaticFiles();
-
-// app.UseRouting();
+app.UseRouting();
 
 // app.UseAuthorization();
 
-// app.MapRazorPages();
+app.MapRazorPages();
 
-// app.Run();
-
-
-namespace RazorPages
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args).Build();
-            // CreateDbIfNotExists(builder);
-            builder.Run();
-        }
-
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var context = services.GetRequiredService<ComputerContext>;
-                context.Database.EnsureCreated();
-            }
-        }
-    }
-}
+app.Run(); // Runs the app.
